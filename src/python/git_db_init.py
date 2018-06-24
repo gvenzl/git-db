f676e753b923f32e97556f8f2da06a3953643f89
Gerald Venzl <gerald.venzl@gmail.com>
2018-06-23 23:45:39 -0700

    Introduce Unit tests
    Signed-off-by: Gerald Venzl <gerald.venzl@gmail.com>

--- src/python/git_db_init.py
+++ src/python/git_db_init.py
@@ -22,52 +22,60 @@
 
 import argparse
 import os
+import sys
 import git_db_database as db
 import git_db_configuration as config
 import git_db_utils as utils
 
-parser = argparse.ArgumentParser(prog="git db init", description="Initialize git repo and database tracking")
-# Currently only Oracle is supported
-# parser.add_argument("--dbtype", choices=["oracle"], required=True, help="Database type")
-parser.add_argument("--user", required=True, help="The database schema to track")
-parser.add_argument("--password", required=True,
-                    help="The database schema password")
-parser.add_argument("--host", required=True,
-                    help="The host name on which the database is running on")
-parser.add_argument("--port", required=True,
-                    help="The port on which the database is listening")
-parser.add_argument("--dbname", required=True,
-                    help="The name of the database")
-parser.add_argument("--role", help="The database user role (SYSDBA, SYSOPER, ...)")
-parser.add_argument("--all", action="store_true",
-                    help="Track all database changes. If set, the database user must have rights "
-                         "to create a database wide trigger.")
-args = parser.parse_args()
-if args.user.upper() == "SYS" and not args.all:
-    parser.error("SYS user requires --all parameter.")
 
-# Test connection
-try:
-    conn = db.connect("oracle", args.user, args.password, args.host, args.port, args.dbname, args.role)
-except ConnectionError as err:
-    utils.print_error("git-db error while connecting to the database:", err)
-    exit(1)
+def run(cmd):
+    parser = argparse.ArgumentParser(prog="git db init", description="Initialize git repo and database tracking")
+    # Currently only Oracle is supported
+    # parser.add_argument("--dbtype", choices=["oracle"], required=True, help="Database type")
+    parser.add_argument("--user", required=True, help="The database schema to track")
+    parser.add_argument("--password", required=True,
+                        help="The database schema password")
+    parser.add_argument("--host", required=True,
+                        help="The host name on which the database is running on")
+    parser.add_argument("--port", required=True,
+                        help="The port on which the database is listening")
+    parser.add_argument("--dbname", required=True,
+                        help="The name of the database")
+    parser.add_argument("--role", help="The database user role (SYSDBA, SYSOPER, ...)")
+    parser.add_argument("--all", action="store_true",
+                        help="Track all database changes. If set, the database user must have rights "
+                             "to create a database wide trigger.")
+    args = parser.parse_args(cmd)
+    if args.user.upper() == "SYS" and not args.all:
+        parser.error("SYS user requires --all parameter.")
 
-# Initialize database (schema)
-try:
-    db.setup("oracle", conn, args.all)
-except Exception as err:
-    utils.print_error("git-db error while setting up database objects:", err)
-    exit(1)
-finally:
-    conn.close()
+    # Test connection
+    try:
+        conn = db.connect("oracle", args.user, args.password, args.host, args.port, args.dbname, args.role)
+    except ConnectionError as err:
+        utils.print_error("git-db error while connecting to the database:", err)
+        return 1
 
-# Run git init on working directory
-try:
-    os.system("git init")
-except OSError as err:
-    utils.print_error("git-db error while initializing git repo:", err)
-    exit(1)
+    # Initialize database (schema)
+    try:
+        db.setup("oracle", conn, args.all)
+    except Exception as err:
+        utils.print_error("git-db error while setting up database objects:", err)
+        return 1
+    finally:
+        conn.close()
 
-# Store database credentials in git config
-config.store_credentials(args.user, args.password, args.host, args.port, args.dbname, args.role)
+    # Run git init on working directory
+    try:
+        os.system("git init")
+    except OSError as err:
+        utils.print_error("git-db error while initializing git repo:", err)
+        return 1
+
+    # Store database credentials in git config
+    config.store_credentials(args.user, args.password, args.host, args.port, args.dbname, args.role)
+    return 0
+
+
+if __name__ == "__main__":
+    exit(run(sys.argv[1:]))
---
+++ test/python/test_git_db_init.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#
+# Since: June, 2018
+# Author: gvenzl
+# Name: test_git_db_init.py
+# Description: Unit test for git_db_init.py
+#
+# Copyright 2018 Gerald Venzl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import unittest
+import shutil
+import git_db_init as init
+
+
+class GitDbInitTestCase(unittest.TestCase):
+    def test_git_db_init(self):
+        """Test initialization of repository"""
+        self.assertEqual(0, init.run(["--user", "test", "--password", "test",
+                                      "--host", "localhost", "--port", "1521", "--dbname", "ORCLPDB1"]))
+
+    def test_git_db_init_already_existing_repo(self):
+        """Test reinitialization of a repository"""
+        self.assertEqual(0, init.run(["--user", "test", "--password", "test",
+                                      "--host", "localhost", "--port", "1521", "--dbname", "ORCLPDB1"]))
+        self.assertEqual(0, init.run(["--user", "test", "--password", "test",
+                                      "--host", "localhost", "--port", "1521", "--dbname", "ORCLPDB1"]))
+
+    def test_negative_init_database_not_running(self):
+        """Negative test of not running database"""
+        self.assertEqual(1, init.run(["--user", "does", "--password", "not",
+                                      "--host", "localhost", "--port", "1521", "--dbname", "exist"]))
+
+    def tearDown(self):
+        self.assertIsNone(shutil.rmtree(".git", True))
+
+
+if __name__ == '__main__':
+    unittest.main()
